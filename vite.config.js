0df95cfd0272ed3e8ec46f6c6240b1feea161587
Isela Alarcón <isela.v.alarcon@gmail.com>
2024-05-28 19:42:56 +0200

    try github action

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
+  base: '/wedding/',
   plugins: [
     react(), viteStaticCopy({
       targets: [
