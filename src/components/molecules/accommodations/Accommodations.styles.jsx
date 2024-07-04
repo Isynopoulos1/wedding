c43ed355e68747269ac5820fe2a1ecf061344117
Isela Alarcón <isela.v.alarcon@gmail.com>
2024-07-04 17:21:33 +0200

    adding constants

--- src/components/molecules/accommodations/Accommodations.styles.jsx
+++ src/components/molecules/accommodations/Accommodations.styles.jsx
@@ -3,33 +3,33 @@ import styled from "styled-components";
 
 export const MainContainer = styled.div`
   width: 100%;
-  max-width: 1500px;
+  max-width: ${contentW.desktop};
   margin: 0 auto;
   p {
     text-align: center;
-    font-size: 24px;
+    font-size: ${sizes.lg};
     line-height: 1.7;
-    padding-bottom: 32px;
+    padding-bottom: ${sizes.xl};
   }
   @media only screen and (max-width: ${contentW.mobile}) {
     p {
-      font-size: 20px;
+      font-size: ${sizes.md};
     }
   }
 `;
 export const AccommodationList = styled.div`
   position: relative;
   width: 100%;
-  max-width: 1500px;
+  max-width: ${contentW.desktop};
   margin: 0 auto;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: auto;
-  column-gap: 10px;
+  column-gap: ${sizes.xss};
   @media only screen and (max-width: ${contentW.mobile}) {
     grid-template-columns: repeat(2, 1fr);
   }
-  @media only screen and (max-width: 450px) {
+  @media only screen and (max-width: ${sizes.super}) {
     grid-template-columns: repeat(1, 1fr);
   }
 
@@ -40,14 +40,14 @@ export const AccommodationList = styled.div`
     align-items: center;
     flex-grow: 0;
     flex-shrink: 1;
-    height: 48px;
+    height: ${sizes.xxll};
     width: calc(100% - 12px);
     width: 100%;
     text-decoration: none;
-    color: black;
+    color: ${colors.black};
     cursor: pointer;
     span {
-      color: black;
+      color: ${colors.black};
       &:hover {
         color: ${colors.silver};
         transition: background-color 0.3s ease;
